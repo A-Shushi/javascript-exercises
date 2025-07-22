4cb4346d8d6b310802633e404425f11328d4b70e
A-Shushi <219606551+A-Shushi@users.noreply.github.com>
2025-07-22 15:41:40 +0700

    Complete task 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+    if (+temperature === 32) {
+        return 0
+    } else {
+        return +((+temperature - 32) * 5 / 9).toFixed(1)
+    }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+    return +((+temperature * 9 / 5) + 32).toFixed(1)
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit
 };
+
+// (0°C × 9/5) + 32 = 32°F
