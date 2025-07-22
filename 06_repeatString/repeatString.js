b91b9a32cbade6e4035d74ea05b0632731a04207
A-Shushi <219606551+A-Shushi@users.noreply.github.com>
2025-07-22 14:11:00 +0700

    Complete Task 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (stringValue, n) {
+    if (n < 0) {
+        return "ERROR"
+    } else {
 
+        let repeatedString = "";
+        for (let i = 0; i < n; i++) {
+            repeatedString += stringValue
+        }
+        return repeatedString
+    }
 };
 
 // Do not edit below this line
