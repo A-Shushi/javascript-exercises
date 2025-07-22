f9b66246ad23ced11dca9c2a0ecbc384a41b2353
A-Shushi <219606551+A-Shushi@users.noreply.github.com>
2025-07-22 14:19:35 +0700

    Complete task 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(stringValue) {
+    let stringArray = stringValue.split("")
+    stringArray.reverse()
+    return stringArray.join("")
 };
+
+reverseString("hello")
 
 // Do not edit below this line
 module.exports = reverseString;
