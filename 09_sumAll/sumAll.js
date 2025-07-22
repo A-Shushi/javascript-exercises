513283f177391c2325b5408f7f225e99ff123c96
A-Shushi <219606551+A-Shushi@users.noreply.github.com>
2025-07-22 15:25:34 +0700

    Complete task 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (firstNumber, secondNumber) {
+    let numberArray = [];
+    if (firstNumber < 0 ||
+        secondNumber < 0 ||
+        firstNumber !== Math.floor(firstNumber) ||
+        secondNumber !== Math.floor(secondNumber)) {
+        return "ERROR";
+    }
+    if (firstNumber > secondNumber) {
+        let temporarySpace = firstNumber;
+        firstNumber = secondNumber;
+        secondNumber = temporarySpace;
+    }
+    for (let i = firstNumber; i <= secondNumber; i++) {
+        numberArray.push(i);
+    }
+    return numberArray.reduce((sum, number) => sum + number, 0);
 };
 
 // Do not edit below this line
