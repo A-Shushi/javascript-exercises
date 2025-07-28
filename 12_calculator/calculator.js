6b4496c280ea0fb1054fb96e9dc12ac01905a6b2
A-Shushi <219606551+A-Shushi@users.noreply.github.com>
2025-07-28 12:24:41 +0700

    Complete task 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,33 +1,43 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+    return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+    return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (numberArray) {
+    return numberArray.reduce((sum, number) => sum += number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numberArray) {
+    return numberArray.reduce((product, number) => product *= number);
 };
 
-const power = function() {
-	
+const power = function (num, power) {
+    return num ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+    if (num === 0) {
+        return 1;
+    } else {
+        let product = 1
+        for (let i = 1; i <= num; i++) {
+            console.log(i)
+            product *= i;
+            console.log(product)
+        }
+        return product
+    }
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };
