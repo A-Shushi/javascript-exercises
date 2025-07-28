958c0ff04d30a58bb7571c4609899efb3b554b3e
A-Shushi <219606551+A-Shushi@users.noreply.github.com>
2025-07-28 12:53:10 +0700

    Complete task 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    let noPunctuationPhrase = phrase.replace(/[^\w\s]|_/g, "");
+    let noSpacesPhrase = noPunctuationPhrase.replace(/\s/g, "")
+    let cleanPhrase = noSpacesPhrase.toLowerCase()
+    let phraseArray = cleanPhrase.split("");
+    let reversedArray = phraseArray.slice().reverse();
+    console.log(phraseArray)
+    console.log(reversedArray)
+    for (let i = 0; i < phraseArray.length; i++) {
+        if (phraseArray[i] !== reversedArray[i]) {
+            return false
+        }
+    }
+    return true
 };
 
 // Do not edit below this line
