c744618b7f1def0e44018258a5888a423076f154
A-Shushi <219606551+A-Shushi@users.noreply.github.com>
2025-07-28 13:43:16 +0700

    Complete task 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+    let ageArray = [];
+    let oldestAge = 0
+    let oldestIndex;
+    for (let person of array) {
+        if (!person.yearOfDeath) {
+            ageArray.push((new Date().getFullYear()) - person.yearOfBirth)
+        } else {
+            ageArray.push(person.yearOfDeath - person.yearOfBirth)
+        }
+    }
+    for (let i = 0; i < ageArray.length; i++) {
+        if (ageArray[i] > oldestAge) {
+            oldestAge = ageArray[i]
+            oldestIndex = i;
+        }
+    }
+    return array[oldestIndex]
 };
 
 // Do not edit below this line
