3540eb65b434e55bfb4d20152dca44495ea9b886
A-Shushi <219606551+A-Shushi@users.noreply.github.com>
2025-07-28 13:07:40 +0700

    Complete task 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 0) {
+        return "OOPS"
+    }
+    let fibonacciArray = [0, 1];
+    for (let i = 2; i <= index; i++) {
+        fibonacciArray.push(fibonacciArray[(i - 2)] + fibonacciArray[(i - 1)])
+    }
+    return fibonacciArray[index]
 };
 
 // Do not edit below this line
